f514ef4c3f141e682b52ae39efb79c4d1f220d0d
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-01-29 06:33:32 +0300

    set up docker and docker-compose

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "geolocations",
 ]
 
 MIDDLEWARE = [
@@ -72,11 +73,23 @@ WSGI_APPLICATION = "geodjango.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'db',  # Name set to our database service in docker-compose
+        'PORT': 5432,
     }
 }
 
